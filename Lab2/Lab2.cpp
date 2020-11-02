235d35452e4bdd03b722c65373d5aea4b72968af
demian_now <demian.now@yandex.ru>
2020-11-02 12:50:41 +0300

    first commit

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int* Get_Rand_Array_Int(size_t size)
+{
+	int* array = new int[size];
+	int sign;
+	for (size_t i = 0; i < size; i++)
+	{
+		sign = 1;
+		if (rand() % 2 == 1)
+			sign = -1;
+		array[i] = rand() * sign;
+	}
+	return array;
+}
+
+int* Get_Rand_Array_Char(size_t size)
+{
+	int* array = new int[size];
+	int sign;
+	for (size_t i = 0; i < size; i++)
+	{
+		sign = 1;
+		if (rand() % 2 == 1)
+			sign = -1;
+		array[i] = rand() * sign;
+	}
+	return array;
+}
+
+double CountingSort(char* array, size_t size)
+{
+	const size_t max_value = 256;
+	clock_t time_test = clock();
+	int temp_array[max_value];
+	for (size_t i = 0; i < max_value; i++)
+		temp_array[i] = 0;
+	for (size_t i = 0; i < size; i++)
+		temp_array[array[i] + 127]+=1;
+	size_t control=0;
+	for (size_t i = 0; i < max_value; i++)
+	{
+		for (int j = 0; j < temp_array[i]; j++)
+		{
+			array[control] = i - 127;
+			control++;
+		}
+	}
+	time_test = clock() - time_test;
+	return ((double)time_test / CLOCKS_PER_SEC * 1000);
+}
+
+double InsertionSort(int* array, size_t size)
+{
+	clock_t time_test = clock();
+	int key; size_t j;
+	for (size_t i = 2; i < size; i++)
+	{
+		key = array[i];
+		j = i - 1;
+		while (j > 0 && array[j] > key)
+		{
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = key;
+	}
+	time_test = clock() - time_test;
+	return ((double)time_test / CLOCKS_PER_SEC * 1000);
+}
+
+void Print_Array(int *array, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+		cout << array[i] << ' ';
+	cout << endl;
+}
+
+double BubbleSort(int *array, size_t size)
+{
+	clock_t time_test = clock();
+	int control;
+	for (size_t i = 0; i < size; i++)
+	{
+		control = 0;
+		for(size_t j=0; j<size-1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+				control++;
+			}
+		if (control == 0)
+			break;
+	}
+	time_test = clock() - time_test;
+    return ((double)time_test/CLOCKS_PER_SEC*1000);
+}
+
+bool issorted(int* array, size_t size)
+{
+	for(size_t i=0; i<size-1; i++)
+		if(array[i]>array[i+1])
+			return false;
+	return true;
+}
+
+bool issorted_char(char* array, size_t size)
+{
+	for (size_t i = 0; i < size - 1; i++)
+		if (array[i] > array[i + 1])
+			return false;
+	return true;
+}
+
+double bogoSort(int* array, size_t size)
+{
+	clock_t time_test = clock();
+	while (!issorted(array, size))
+		for (size_t i = 0; i < size; i++)
+			swap(array[i], array[rand() % size]);
+	time_test = clock() - time_test;
+	return ((double)time_test / CLOCKS_PER_SEC * 1000);
+}
+
+double QuickSort(int* array, int size) 
+{
+	clock_t time_test = clock();
+	int i = 0, j = size - 1, mid = array[size / 2];
+	do {
+		while (array[i] < mid) i++;
+		while (array[j] > mid) j--;
+		if (i <= j) {
+			swap(array[i], array[j]);
+			i++;
+			j--;
+		}
+	} while (i <= j);
+	if (j > 0) QuickSort(array, j + 1);
+	if (i < size) QuickSort(&array[i], size - i);
+	time_test = clock() - time_test;
+	return ((double)time_test / CLOCKS_PER_SEC * 1000);
+}
+
+size_t BinarySearch(int* array, size_t size, int key)
+{
+	size_t mid = size / 2, left = 0, right = size - 1;
+	if (!issorted(array, size))
+		throw "The array is not sorted";
+	while (array[mid] != key)
+	{
+		mid = (left + right) / 2;
+		if (key < array[mid])
+			right = mid - 1;
+		else if (key > array[mid])
+			left = mid + 1;
+		else
+			return mid;
+		if (left > right)
+			throw "Element not found";
+	}
+}
+
+int main()
+{
+	srand(time(0));
+	int size = 10; 
+	double current_time;
+	int* test;
+	for (int i = 0; i < 5; i++)
+	{
+		current_time = 0;
+		for (int j = 0; j < 10; j++)
+		{
+			test = Get_Rand_Array_Int(size);
+			cout << QuickSort(test, size) << " ";
+			delete[] test;
+		}
+		cout << "for " << size << " elements" << endl;
+		size *= 10;
+	}
+	size = 10;
+	for (int i = 0; i < 5; i++)
+	{
+		current_time = 0;
+		for (int j = 0; j < 10; j++)
+		{
+			test = Get_Rand_Array_Int(size);
+			cout << BubbleSort(test, size) << " ";
+			delete[] test;
+		}
+		cout << "for " << size << " elements" << endl;
+		size *= 10;
+	}
+
+}
+
+
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,105 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab2\Lab2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(issorted_true)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,2,3,4 };
+			Assert::AreEqual(issorted(test, size), true);
+		}
+		TEST_METHOD(issorted_false)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			Assert::AreEqual(issorted(test, size), false);
+		}
+		TEST_METHOD(issorted_char_true)
+		{
+			const size_t size = 5;
+			char test[size] = {1,2,3,4,5};
+			Assert::AreEqual(issorted_char(test, size), true);
+		}
+		TEST_METHOD(issorted_char_false)
+		{
+			const size_t size = 5;
+			char test[size] = "leti";
+			Assert::AreEqual(issorted_char(test, size), false);
+		}
+		TEST_METHOD(BogoSort_Test)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			bogoSort(test, size);
+			Assert::AreEqual(issorted(test, size), true);
+		}
+		TEST_METHOD(BubbleSort_Test)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			BubbleSort(test, size);
+			Assert::AreEqual(issorted(test, size), true);
+		}
+		TEST_METHOD(QuickSort_Test)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			QuickSort(test, size);
+			Assert::AreEqual(issorted(test, size), true);
+		}
+		TEST_METHOD(InsertionSort_Test)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			InsertionSort(test, size);
+			Assert::AreEqual(issorted(test, size), true);
+		}
+		TEST_METHOD(CountingSort_Test)
+		{
+			const size_t size = 10;
+			char test[size] = {5,4,3,2,1,1,2,3,4,5};
+			CountingSort(test, size);
+			Assert::AreEqual(issorted_char(test,size), true);
+		}
+		TEST_METHOD(BinarySearch_NotSortedArray)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,20,3,4 };
+			try 
+			{
+				BinarySearch(test, size, 1);
+			}
+			catch (const char* error)
+			{
+				Assert::AreEqual(error, "The array is not sorted");
+			}
+		}
+		TEST_METHOD(BinarySearch_ErrorKey)
+		{
+			const size_t size = 5;
+			int test[size] = { 0,1,2,3,4 };
+			BinarySearch(test, size, 1);
+			try {
+				BinarySearch(test, size, 5);
+			}
+			catch (const char* error)
+			{
+				Assert::AreEqual(error, "Element not found");
+			}
+		}
+		TEST_METHOD(BinarySearch_Normal)
+		{
+			const size_t size = 5, correct_index = 2;
+			int test[size] = { 1,3,5,7,9 };
+			Assert::AreEqual(BinarySearch(test, size, 5),correct_index);
+		}
+	};
+}
